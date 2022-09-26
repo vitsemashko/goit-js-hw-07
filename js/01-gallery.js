c0837e961e3575272279dba9eeef8af5cc361b7d
vitsemashko <107743238+vitsemashko@users.noreply.github.com>
2022-09-26 11:37:34 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,5 +1,6 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+import SimpleLightbox from "/simplelightbox/dist/simple-lightbox";
 
 console.log(galleryItems);
 
