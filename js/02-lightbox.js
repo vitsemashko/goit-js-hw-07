423229698d98a5b368e56fde08a49af12188cdaa
vitsemashko <107743238+vitsemashko@users.noreply.github.com>
2022-11-05 08:59:35 +0200

    Update 02-lightbox.js
    Removed extra code

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,10 +1,6 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
-
-console.log(galleryItems);
-
 const gallery = document.querySelector(".gallery");
 const items = [];
 
